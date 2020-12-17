049ef5905e616bc62f100374ca70db24398ba34d
Kevin Moulun <Atnnetworkk@gmail.com>
2020-12-17 19:41:18 +0100

    Add homepage responsive & cardmaker

--- src/components/cardMaker.js
+++ src/components/cardMaker.js
@@ -1,29 +1,39 @@
 import React from "react";
+import MultipleItems from "./cardPicler";
 import Header from "./header";
 
-const CardMaker = () => {
+const CardMakerForm = () => {
   return (
-    <main className="card-maker">
+    <main className="card-main col-lg-6">
       <Header />
-      <label htmlFor="sender" className="card-maker__label">
-        What’s your name ?
-      </label>
-      <input type="text" name="" id="sender" className="card-maker__input" />
-      <label htmlFor="receiver" className="card-maker__label">
-        What’s the receiver name ?
-      </label>
-      <input type="text" name="" id="receiver" className="card-maker__input" />
-      <label htmlFor="message" className="card-maker__label">
-        What’s the special message ?
-      </label>
-      <textarea
-        type="text"
-        name=""
-        id="message"
-        className="card-maker__input"
-      />
+      <div className="card-maker">
+        <label htmlFor="sender" className="card-maker__label">
+          What’s your name ?
+        </label>
+        <input type="text" name="" id="sender" className="card-maker__input" />
+        <label htmlFor="receiver" className="card-maker__label">
+          What’s the receiver name ?
+        </label>
+        <input
+          type="text"
+          name=""
+          id="receiver"
+          className="card-maker__input"
+        />
+        <label htmlFor="message" className="card-maker__label">
+          What’s the special message ?
+        </label>
+        <textarea
+          type="text"
+          name=""
+          id="message"
+          className="card-maker__input --last"
+        />
+        <button className="card-maker__btn">Next step</button>
+      </div>
+      <MultipleItems />
     </main>
   );
 };
 
-export default CardMaker;
+export default CardMakerForm;
--- src/components/homepage.js
+++ src/components/homepage.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <main className="homepage">
+    <main className="homepage col-12">
       <div className="row">
         <div className="col-lg-6">
           <div className="homepage__left">
@@ -25,7 +25,7 @@ const HomePage = () => {
           </p>
           <div className="button-container">
             <p className="homepage__button">
-              <Link to="/create">Home</Link>
+              <Link to="/create">Create my card</Link>
             </p>
           </div>
         </div>
