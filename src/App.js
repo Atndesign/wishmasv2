5e43f98e815c6de11cc195e0408eebe2820a8f63
Kevin Moulun <Atnnetworkk@gmail.com>
2020-12-17 17:21:35 +0100

    add responsive

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./assets/css/App.css";
 import HomePage from "./components/homepage";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+          <Route path="/create" exact>
+            <div>HJellos</div>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,24 +1,32 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <main className="homepage">
       <div className="row">
-        <div className="col-lg-6 text-center">
-          <img
-            className="homepage__img"
-            src="./img/christmas-tree.png"
-            alt="Tree"
-            srcset="./img/christmas-tree.svg"
-          />
-          <h1 className="homepage__title">Christmas</h1>
-          <p className="homepage__subtitle">Card generator</p>
-        </div>
         <div className="col-lg-6">
+          <div className="homepage__left">
+            <img
+              className="homepage__img"
+              src="./img/christmas-tree.png"
+              alt="Tree"
+              srcset="./img/christmas-tree.svg"
+            />
+            <div className="left__content">
+              <h1 className="homepage__title">Christmas</h1>
+              <p className="homepage__subtitle">Card generator</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-6 homepage__right">
+          <p className="homepage__text">
+            Tell how much you love the persons close to you
+          </p>
           <div className="button-container">
-            <a href="/create" className="homepage__button">
-              Create my card
-            </a>
+            <p className="homepage__button">
+              <Link to="/create">Home</Link>
+            </p>
           </div>
         </div>
       </div>
